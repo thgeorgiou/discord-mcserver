1543c2e4466cdc759cc5177e15ac4381e5b5f3ad
Thanasis Georgiou <contact@thgeorgiou.com>
2020-03-20 22:07:27 +0200

    Gracefully handle keyboard interrupt

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,20 @@ const client = new Client();
 const server = new MinecraftServer(process.env["MCSERVER_TERRAFORM"]);
 const bot = new Bot(client, server, process.env["MCSERVER_CHANNELID"]);
 
+// Catch keyboard interrupt
+process.on("SIGINT", () => {
+  console.log("Exiting on interrupt signal!");
+  client.destroy();
+  process.exit(0);
+});
+
+// Handle successful connection
 client.on("ready", async () => {
   console.log("[Client] Connected to discord!");
   bot.updatePresence();
 });
 
+// Log any discord API errors
 client.on("error", err => {
   console.log("[Client] Error occured!");
   console.log(JSON.stringify(err));
